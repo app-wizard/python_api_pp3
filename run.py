6f8b6db9e8741dab0bd7a888dc520179a6a6c505
app-wizard <>
2023-09-13 15:44:15 +0000

    add weather api

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import gspread
+import requests 
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -12,4 +13,40 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('pp3sheet')
 
-print("Welcome to My Project")
+API_URL = "https://api.openweathermap.org/data/2.5/weather?"
+API_KEY = open("./api.key", "r").read()
+city = "DUBLIN"
+
+def get_weather(city):
+    print("_______________________________________________________")
+    url = API_URL+"q="+city+',&APPID='+API_KEY
+    print(f"Welcome today we have wonderful weather! In {city}:")
+    response = requests.get(url).json()
+    temp = round(response['main']['temp'] - 273.15)
+    humidity = response['main']['humidity']
+    descr = response['weather'][0]['description']
+    print(f"Temperature: {temp}°C  *  Humidity: {humidity}%  * {descr} ")
+
+def new_city(answer):
+    if answer == "Y":
+        print("Enter Your City:")
+        newcity = input().upper()
+        get_weather(newcity)
+    elif answer =="N":
+            print("OK Let's go!")
+    else:
+        print('The answer is accepted only yes -"Y" or no- "N", please enter your answer again: \n')
+        resp = input().upper()
+        new_city(resp)
+
+def main():
+    """
+    Run all program functions
+    """
+    get_weather(city)
+    print("_______________________________________________________")
+    print("Do you want to change the city Y/N? \n")
+    resp = input().upper()
+    new_city(resp)
+
+main()
