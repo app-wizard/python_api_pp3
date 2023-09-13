b8e9287fd515fb682d5340d2fc6c0a9ca5e485f0
app-wizard <>
2023-09-13 16:36:33 +0000

    add clear terminal  function

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 import requests 
+import os
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -18,24 +19,37 @@ API_KEY = open("./api.key", "r").read()
 city = "DUBLIN"
 
 def get_weather(city):
+    clear()
     print("_______________________________________________________")
     url = API_URL+"q="+city+',&APPID='+API_KEY
-    print(f"Welcome today we have wonderful weather! In {city}:")
     response = requests.get(url).json()
+    print(f"Welcome today we have wonderful weather! In {city}:")
     temp = round(response['main']['temp'] - 273.15)
     humidity = response['main']['humidity']
     descr = response['weather'][0]['description']
     print(f"Temperature: {temp}°C  *  Humidity: {humidity}%  * {descr} ")
+    print("_______________________________________________________")
+
+def clear():
+    os.system('clear')
 
 def new_city(answer):
     if answer == "Y":
         print("Enter Your City:")
         newcity = input().upper()
-        get_weather(newcity)
+        try:
+            get_weather(newcity)
+        except:
+            clear()
+            print(f"Something went wrong, maybe just a typo in the name of the {newcity}, try again:")
+            print("Do you want to change the city Y/N? \n")
+            resp = input().upper()
+            new_city(resp)
+
     elif answer =="N":
-            print("OK Let's go!")
+        pass             
     else:
-        print('The answer is accepted only yes -"Y" or no- "N", please enter your answer again: \n')
+        print('The answer is accepted only yes -"Y" or no- "N", please enter Your answer again: \n')
         resp = input().upper()
         new_city(resp)
 
@@ -44,9 +58,9 @@ def main():
     Run all program functions
     """
     get_weather(city)
-    print("_______________________________________________________")
     print("Do you want to change the city Y/N? \n")
     resp = input().upper()
     new_city(resp)
-
+    
+    print("OK Let's go!")
 main()
